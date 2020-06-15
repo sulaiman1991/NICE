269b169ccb8fe855532b2f2cd4c6d117e2bfe1f0
sulaiman1991 <64702660+sulaiman1991@users.noreply.github.com>
2020-06-15 23:58:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,321 @@
+//alert("hello world");
+
+//var a = 5;
+//var b = 10;
+//var c = a+b;
+//alert(c)
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// console.log(c); 
+
+// var x=5;
+// var y="ali";
+// var z =y+x;
+// alert(z);
+
+// var a = 3;
+// var b = 5;
+// var c = 7;
+// var d = (b-a) * c;
+// alert(d); 
+
+// var a = 5;
+// var b = ++a
+// alert(b);
+
+// var x = 10
+// var y = x++ + ++x - x + x++ - ++x + x;
+// var z = alert(y);
+
+
+// 
+
+// var age = +prompt("enter your age");
+// var gender = prompt("enter your gender")
+// if(age > 18 || gender == "male"){
+//     alert ("ou are allowed to ride")
+// }
+// else{
+//     alert ("not allowed")
+// }
+
+// var a = "5";
+// if(a===5){
+//     alert("true")
+// }
+// else{
+//     alert("false")
+
+// }
+
+// var x = 5;
+// var y = 10;
+// var a = 30;
+// var b = 15;
+// if(x+y === a-b){
+//     alert("correct")
+// }
+// else{
+//     alert("incorrect")
+// }
+
+// var a = 10
+// if(a !==15){
+//     document.write("correct")
+// }
+// else{
+//     document.write("incorrect")
+// }
+
+// var percent = +prompt("enter your percent")
+// if(percent >= 80 && percent <= 100){
+//     alert ("A+")
+// }
+// else if (percent >= 70 && percent <= 80){
+//     alert("A")
+// }
+// else if (percent >= 60 && percent <= 70){
+//     alert("B")
+// }
+// else if (percent >= 50 && percent <= 60){
+//     alert("C")
+// }
+// else if (percent >= 40 && percent <= 50){
+//     alert("D")
+// }
+
+// else if (percent >= 33 && percent <= 40){
+//     alert("E")
+// }
+// else if (percent >= 0 && percent <= 33){
+//     alert("F")
+// }
+// else{
+//     alert("you didn't write correct %age")
+// }
+
+
+// var student =["sul","dasd","aff"]
+// console.log(sul)
+
+// alert ("welcome" + sul)
+
+// students.shift( )
+
+// students.unshift("")
+
+// var students =[ "sul","fai","nas", "foz"]
+
+// students.splice()
+
+// var copyStudents = students.slice()
+
+// console.log(students)
+// console.log(copyStudents)
+
+//Chapter # 01 
+
+alert("Greeting");
+
+alert("Error! Please enter a valid password");
+
+alert("welcome to JS land")
+
+var text =("Hello..I can run JS through my browser console");
+console.log(text)
+
+//Chapter # 02
+
+var Name = "username";
+
+
+var Name = "myname";
+var str = 'Sulaiman';	
+    
+
+
+var title = "JS Variable";
+
+
+var Name = "John Doe";
+var age = "15 yrs old";
+var Expertise = "Certified MobileApp Development";
+
+var
+  ONE
+  =
+PIZZA;
+var
+  two
+  =
+PIZZ;
+var
+three
+  =
+PIZ;
+var
+  four
+  =
+PI;
+var
+  five
+  =
+P;
+
+var email = "emailaddress";
+var str = 'My ' + 'emailaddress ' + 'is ' + 'example@example.com ';
+
+var text =("A smarter way to learn JS");
+Document.WRITE(text)
+
+
+var text =("yAH! I can write HTML content through JS");
+console.log(text)
+
+
+
+//Chapter # 03
+
+ var age = +prompt("enter your age");
+  alert("i am 15 years Old")
+
+
+  var birthyear = ("My birthyear is 1990");
+  console.log(birthyear);
+
+
+<script type="John Doe ordered 5 T-shirt(s) on XYZ Clothing store">
+
+</script>
+
+//Chapter # 04
+
+var variable1 = "Hello World!";
+var variable2 = "Testing";
+var variable3 = 30;
+
+var $_;
+
+//Chapter # 05
+
+var a = 3;
+ var b = 5;
+ var c = a+b
+ alert(c);
+
+ var a = 3;
+ var b = 5;
+ var c = a*b
+ alert(c);
+
+ var a = 3;
+ var b = 5;
+ var c = a/b
+ alert(c);
+
+
+ var a = 5;
+ var b = 3;
+ var c = a-b
+ alert(c);
+
+ <script ticketprice="Total cost to buy 5 tickets to a movie is 5000PKR">
+
+</script>
+
+<script type ="Table of 4">
+var  t=4;
+var  i=1;
+while(i<=10)
+{
+document.write(4+  "  X  "  +1+  "  =  "  +1*4+   "<br>");
+1++;
+}
+
+</script>
+
+
+var Priceitem1(a) =650 ;
+var Priceitem2(b) =100 ;
+var Ordered quantitY of item1 (c) =3;
+var Ordered quantitY of item2 (d)=7;
+Var shipping charges (e)=100;
+var (f) = (a)+(b)+(c)+(d)+(e)
+ alert(f);
+
+
+ var a = 10
+ var b = ++a + a++ + --x + x--;
+  var c = alert(b);
+
+//cHAPTER 9-10
+
+
+var age = +prompt("enter your age");
+ var gender = prompt("enter your gender")
+if(age > 18 || gender == "male"){
+    alert ("ou are allowed to ride")
+}
+else{
+    alert ("not allowed")
+}
+
+var a = "5";
+if(a===5){
+    alert("true")
+}
+else{
+    alert("false")
+
+}
+
+
+var percent = +prompt("enter your percent")
+if(percent >= 80 && percent <= 100){
+    alert ("A+")
+}
+else if (percent >= 70 && percent <= 80){
+    alert("A")
+}
+else if (percent >= 60 && percent <= 70){
+    alert("B")
+}
+else if (percent >= 50 && percent <= 60){
+    alert("C")
+}
+else if (percent >= 40 && percent <= 50){
+    alert("D")
+}
+
+else if (percent >= 33 && percent <= 40){
+    alert("E")
+}
+else if (percent >= 0 && percent <= 33){
+    alert("F")
+}
+else{
+    alert("you didn't write correct %age")
+}
+
+
+
+
+
+var age = +prompt("enter your age");
+var gender = prompt("enter your gender")
+if(age > 18 || gender == "male"){
+    alert ("ou are allowed to ride")
+}
+else{
+    alert ("not allowed")
+}
+
+
+
+
+
+
+
+
